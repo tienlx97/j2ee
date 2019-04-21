fec62dd6a3845add6aeb79cd238b2205ddc7f967
Tien Xuan <tienlx97@gmail.com>
2019-04-21 10:33:23 +0700

    Fix: database not work

--- database/tables.sql
+++ database/tables.sql
@@ -3,20 +3,20 @@ DROP DATABASE IF EXISTS J2EE;
 CREATE DATABASE IF NOT EXISTS J2EE;
 USE J2EE;
 
-ALTER DATABASE j2ee CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
 DROP TABLE IF EXISTS CATEGORIES;
 CREATE TABLE CATEGORIES (
-	id 				serial 			not null primary key,
+	id 				integer 		not null auto_increment ,
     name 			varchar(254)	not null,
     parentId		integer			,
     createdAt		timestamp 		not null,
-    updatedAt 		timestamp		not null
+    updatedAt 		timestamp		not null,
+    
+    constraint fk_category primary key(id)
 ) ;
 
 DROP TABLE IF EXISTS PRODUCT;
 CREATE TABLE PRODUCT (
-	id 				serial 			not null primary key,
+	id 				integer 		not null auto_increment,
     sku 			varchar(20) 	not null, 
     name			varchar(255)	not null,
     description 	text					,
@@ -29,64 +29,78 @@ CREATE TABLE PRODUCT (
 	detailImages	varchar(1000)	default 'url image',     
     
     createdAt		timestamp 		not null,
-    updatedAt 		timestamp		not null
+    updatedAt 		timestamp		not null,
+    
+    constraint fk_product primary key(id)
 );
 
 DROP TABLE IF EXISTS PRODUCT_CATEGORIES;
 CREATE TABLE PRODUCT_CATEGORIES (
-	categoriesId 	integer 		not null primary key,
-    productId 		integer			not null primary key,
+	categoriesId 	integer 		not null,
+    productId 		integer			not null,
     createdAt		timestamp 		not null,
-    updatedAt 		timestamp		not null
+    updatedAt 		timestamp		not null,
+    
+    constraint fk_product_category primary key(categoriesId,productId)
 );
 
 DROP TABLE IF EXISTS TAG;
 CREATE TABLE TAG (
-	id 				serial 			not null primary key, 
+	id 				integer 		not null auto_increment, 
     name 			varchar(255)	not null,
     createdAt		timestamp 		not null,
-    updatedAt 		timestamp		not null
+    updatedAt 		timestamp		not null,
+    
+	constraint fk_tag primary key(id)
 );
 
 DROP TABLE IF EXISTS PRODUCT_TAG;
 CREATE TABLE PRODUCT_TAG (
-	tagId 			integer 			not null,
+	tagId 			integer 		not null,
 	productId		integer			not null, 		
     createdAt		timestamp 		not null,
     updatedAt 		timestamp		not null,
-    primary key (tagId,productId)
+    constraint fk_product_tag primary key (tagId,productId)
 );
 
 DROP TABLE IF EXISTS CITY;
 CREATE TABLE CITY (
-	id				serial 			not null primary key,
-    name			varchar(20)	not null
+	id			integer 	not null auto_increment,
+    name			varchar(20)	not null,
+    constraint fk_city primary key(id)
 );
 
 DROP TABLE IF EXISTS WARD;
 CREATE TABLE WARD (
-	id				serial 			not null primary key,
-    name			varchar(20)	not null
+	id				integer 	not null auto_increment,
+    name			varchar(20)	not null,
+	constraint fk_ward primary key(id)
 );
 
 DROP TABLE IF EXISTS DISTRICT;
 CREATE TABLE DISTRICT (
-	id				serial 			not null primary key,
-    name			varchar(20)	not null
+	id				integer 	not null auto_increment,
+    name			varchar(20)	not null,
+	constraint fk_district primary key(id)
 );
 
-DROP TABLE IF EXISTS ADDRESS1;
-CREATE TABLE ADDRESS1 (
-	id 				integer 	not null primary key,
-    cityId 			integer		not null FOREIGN KEY REFERENCES CITY(id), 
-    districtId 		integer		not null foreign references DISTRICT(id), 
-    wardId			integer		not null foreign references WARD(id), 
+DROP TABLE IF EXISTS ADDRESS;
+CREATE TABLE ADDRESS (
+	id 				integer 	not null auto_increment,
+    cityId 			integer		not null, 
+    districtId 		integer		not null, 
+    wardId			integer		not null, 
     address			varchar(100)	not null,
+    
+    constraint fk_address primary key(id),
+    constraint fk_city_a FOREIGN KEY(cityId) references CITY(id),
+    constraint fk_district_a FOREIGN KEY(districtId) references DISTRICT(id),
+	constraint fk_ward_a FOREIGN KEY(wardId) references WARD(id)
 );
 
 DROP TABLE IF EXISTS CUSTOMER;
 CREATE TABLE CUSTOMER (
-	id 				serial 			not null,
+	id 				integer 		not null auto_increment,
 	email			varchar(255)	not null,
     username		varchar(20)		not null,
     password 		varchar(255)	not null,
@@ -103,27 +117,27 @@ CREATE TABLE CUSTOMER (
 	createdAt		timestamp 		not null,
     updatedAt 		timestamp		not null,
     
-    primary key(id),
-    foreign key (addressId) references ADDRESS(id)
+    constraint fk_customer primary key(id),
+    constraint fk_address_c foreign key (addressId) references ADDRESS(id)
 );
 
 DROP TABLE IF EXISTS CUTOMER_LOG;
 CREATE TABLE CUSTOMER_LOG(
-	id 				serial			not null,
-	customerId 		serial 			not null,
+	id 				integer			not null auto_increment,
+	customerId 		integer 			not null,
     formName		enum('1','2','3') not null,
     action			nvarchar(100)	not null,
     
 	createdAt		timestamp 		not null,
     updatedAt 		timestamp		not null,
         
-    primary key(id, customerId) 
+    constraint fk_customer_log primary key(id, customerId) 
 );
 
 DROP TABLE IF EXISTS ORDER_PRODUCT;
 CREATE TABLE ORDER_PRODUCT(
-	id 				serial			not null,
-    customerId		serial			not null, /**/
+	id 				integer			not null auto_increment,
+    customerId		integer			not null, /**/
     total			numeric			not null,
 	isDelivery		bool			default false,
     deliveryAt		date			not null,
@@ -131,32 +145,32 @@ CREATE TABLE ORDER_PRODUCT(
     createdAt		timestamp 		not null,
     updatedAt 		timestamp		not null,
     
-    primary key(id),
-    foreign key(customerId) references CUSTOMER(id)
+    constraint fk_product primary key(id),
+    constraint fk_customer_op foreign key(customerId) references CUSTOMER(id)
 );
 
 DROP TABLE IF EXISTS ORDER_DETAIL;
 CREATE TABLE ORDER_DETAIL(
-	id 				serial			not null,
-    orderId			serial			not null, /**/
-    productId		numeric			not null, /**/
+	id 				integer			not null auto_increment,
+    orderId			integer			not null, /**/
+    productId		integer			not null, /**/
     quantity		int(10)			not null, 
     
     createdAt		timestamp 		not null,
     updatedAt 		timestamp		not null,
     
-    primary key(id),
-    foreign key(orderId) references ORDER_PRODUCT(id),
-    foreign key(productId) references PRODUCT(id)
+    constraint fk_order_detail primary key(id),
+    constraint fk_order_od foreign key(orderId) references ORDER_PRODUCT(id),
+    constraint fk_product_od foreign key(productId) references PRODUCT(id)
 );
 
 DROP TABLE IF EXISTS EMPLOYEE;
 CREATE TABLE EMPLOYEE(
-	id 				serial			not null,
+	id 				integer			not null auto_increment,
     username		varchar(15)		not null,
     password		varchar(100)	not null,
-	firstname		varchar(20)	not null,
-    lastname		varchar(20)	not null,    
+	firstname		varchar(20)		not null,
+    lastname		varchar(20)		not null,    
     dob				date			not null,
 	image			varchar(100)	default 'url image',     
     gender			enum('0','1','2')	default '2',
@@ -168,29 +182,29 @@ CREATE TABLE EMPLOYEE(
 	createdAt		timestamp 		not null,
     updatedAt 		timestamp		not null,
     
-    primary key(id),
-    foreign key(addressId) references ADDRESS(id)
+    constraint fk_employee primary key(id),
+    constraint fk_address_e foreign key (addressId) references ADDRESS(id)
 );
 
 DROP TABLE IF EXISTS ROLES;
 CREATE TABLE ROLES(
-	id 				serial			not null,
+	id 				integer			not null auto_increment,
     name			varchar(50)	    not null,
     
     createdAt		timestamp 		not null,
     updatedAt 		timestamp		not null,
     
-    primary key (id)
+    constraint fk_role primary key (id)
 );
 
 DROP TABLE IF EXISTS EMPLOYEE_ROLES;
 CREATE TABLE EMPLOYEE_ROLES(
-	roleId 				serial			not null,
+	roleId 				integer			not null,
     employeeId			varchar(50)	    not null,
     
     createdAt		timestamp 		not null,
     updatedAt 		timestamp		not null,
     
-	primary key (roleId, employeeId)
+	constraint fk_employee_role primary key (roleId, employeeId)
 );
 
